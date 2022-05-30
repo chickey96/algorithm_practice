59cf7380f8f75e7688519f067386fb4dcce3e1ac
corinne <c.m.hickey06@gmail.com>
2022-05-29 23:53:04 -0700

    Write function to find nth fibonacci number in sequence

--- recursive_functions.js
+++ recursive_functions.js
@@ -5,8 +5,20 @@ function calculate_factorial(n) {
     return (n * calculate_factorial(n - 1));
 }
 
+console.log("basic tests for calculate_factorial")
 // factorial 5 = 5! = (5 * 4 * 3 * 2 * 1) = 120
 console.log(`${calculate_factorial(5)} should be 120`)
-
 // factorial 4 = 4! = (4 * 3 * 2 * 1) = 24
 console.log(`${calculate_factorial(4)} should be 24`)
+
+function find_fibonacci(n) {
+    if(n <= 2) { return 1; }
+
+    return (find_fibonacci(n-1) + find_fibonacci(n-2));
+}
+
+console.log("basic tests for find_fibonacci")
+// fibonacci 5 => [1, 1, 2, 3, 5] = 5
+console.log(`${find_fibonacci(5)} should be 5`)
+// fibonacci 5 => [1, 1, 2, 3, 5, 8, 13] = 5
+console.log(`${find_fibonacci(7)} should be 13`)

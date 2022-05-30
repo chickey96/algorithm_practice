ccd194db735b639c9823681d351c3b778d3c0f0a
corinne <c.m.hickey06@gmail.com>
2022-05-30 00:25:14 -0700

    Complete first four practice recursive functions

--- recursion_project/lib/problems.js
+++ recursion_project/lib/problems.js
@@ -17,7 +17,10 @@
 // lucasNumber(5)   // => 11
 // lucasNumber(9)   // => 76
 function lucasNumber(n) {
+    if(n == 0) { return 2; }
+    if(n == 1) { return 1; }
 
+    return (lucasNumber(n - 1) + lucasNumber(n - 2));
 }
 
 
@@ -33,7 +36,9 @@ function lucasNumber(n) {
 // sumArray([5, 2])         // => 7
 // sumArray([4, 10, -1, 2]) // => 15
 function sumArray(array) {
-
+    if(array.length == 0) { return 0; }
+    
+    return (array.pop() + sumArray(array));
 }
 
 
@@ -49,7 +54,9 @@ function sumArray(array) {
 // reverseString("internet")    // => "tenretni"
 // reverseString("friends")     // => "sdneirf"
 function reverseString(str) {
+    if(str.length <= 1) { return str; }
 
+    return reverseString(str.slice(1)) + str[0];
 }
 
 
@@ -70,7 +77,13 @@ function reverseString(str) {
 // pow(3, 4)    // => 81
 // pow(2, -5)   // => 0.03125
 function pow(base, exponent) {
-
+    if(exponent == 0) { return 1; }
+    
+    if(exponent > 0) {
+        return base * pow(base, exponent - 1);
+    } else {
+        return 1 / (base * pow(base, Math.abs(exponent + 1)));
+    }
 }
 
 

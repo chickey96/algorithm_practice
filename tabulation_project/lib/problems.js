626a2da54b0ca9673640aeae7edf2cb11ca0f7ed
corinne <c.m.hickey06@gmail.com>
2022-05-31 02:50:46 -0700

    Write iterative and recursive solutions for maxAdjacentSum problem

--- tabulation_project/lib/problems.js
+++ tabulation_project/lib/problems.js
@@ -78,11 +78,75 @@ function tabulationStepper(nums) {
 // Try to solve this in two ways, using tabulation and memoization.
 //
 // Examples:
-//
-// maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
-// maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
-function maxNonAdjacentSum(nums) {
+// maxNonAdjacentSum([2, 7, 9, 3 , 4])       // => 15, because 2 + 9 + 4
+// delete around max [2,7,9,3,4] => [2,X,9,X,4]
+// solution length is half input length
+// nums = [2,7,9,3,4] solution =[ , , ]
+// always round up? what if nums = [2,9,1,1,5]
+// don't need to know solution length
+// nums = [2,7,9,3,4], chosen = [9] nums = [2,X,X,X,4], chosen = [9, 4] nums = [2,X,X,X,X]
+// don't need to store nums can use sum var
+// don't need to reiterate through nums, can have nums left var
 
+// maxNonAdjacentSum([4, 2, 1, 6])          // => 10, because 4 + 6 
+// delete around max [4,2,1,6] => [4,X,X,6]
+
+// maxNonAdjacentSum([1, 2, 3, 1])          // => 4, because 1 + 3
+// delete around max [1,2,3,1] => [1,X,3,X]
+
+// maxNonAdjacentSum([4, 1, 1 , 10, 3, 2])   // => 16, because 4 + 10 + 2
+// delete around max [4,1,1,10,3,2] => [4,X,X,10,X,2]
+// maxNonAdjacentSum([])                    // => 0
+function maxNonAdjacentSum(nums) {
+    return recursiveMaxAdjacentSum(nums) && iterativeMaxAdjacentSum(nums);
+}
+
+function recursiveMaxAdjacentSum(nums, solved={}) {
+    if(nums.length == 1) return nums[0];
+    if(nums.length == 0) return 0 ;
+    if(nums in solved)   return solved[nums];
+
+    let option1 = nums[0] + recursiveMaxAdjacentSum(nums.slice(2), solved);
+    let option2 = recursiveMaxAdjacentSum(nums.slice(1), solved);
+
+    if(option1 >= option2){
+        solved[nums] = option1;
+    } else {
+        solved[nums] = option2;
+    }
+
+    return solved[nums];
+}
+
+function iterativeMaxAdjacentSum(nums) {
+    let sum = 0;
+    let nums_left = nums.length;
+    const length = nums.length;
+
+    while(nums_left > 0) {
+        let max = Math.max(...nums);
+        let idx = nums.indexOf(max);
+
+        sum += max
+        nums[idx] = -1
+        nums_left --
+
+        // replace positive adjacent elements with a negative placeholder  
+        let left_idx = idx - 1
+        let right_idx = idx + 1
+
+        if(left_idx >= 0 && nums[left_idx] >= 0) {
+            nums[left_idx] = -1
+            nums_left --
+        }
+
+        if(right_idx <= length - 1 && nums[right_idx] >= 0) {
+            nums[right_idx] = -1
+            nums_left --
+        }
+    }
+
+    return sum;
 }
 
 
